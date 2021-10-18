2a96283675febb5353d1a4b96309ffe679089c9a
Derimar Gray <derimargray@gmail.com>
2021-10-18 17:04:46 -0500

    add modal

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -1,27 +1,26 @@
 // __tests__/Gallery.test.js
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Gallery from "..";
-const portrait = {
-  name: "portraits",
-  description: "Portraits of people in my life",
-};
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Gallery from '..'
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
 
-afterEach(cleanup);
 
-describe("Gallery is rendering", () => {
-  it("renders1", () => {
+afterEach(cleanup)
+
+describe('Gallery component', () => {
+
+  it('renders', () => {
     render(<Gallery currentCategory={portrait} />);
   });
 
-  it("renders2", () => {
-    const { asFragment } = render(<Gallery currentCategory={portrait} />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Gallery currentCategory={portrait} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
 });
 
-it("renders3", () => {
-  const { getByTestId } = render(<Gallery currentCategory={portrait} />);
-  expect(getByTestId("h1tag")).toHaveTextContent("Portraits");
-});
+it('displays text', () => {
+  const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+})
